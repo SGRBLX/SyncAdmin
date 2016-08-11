c1fe5bcde1d4e7ff6bf3aa69c9df48cd1920ac9a
Hannah Jane <hannah.jane.lister@gmail.com>
2016-08-11 22:48:41 +0100

    Updated QuickScript
    Patched security issues regarding the SyncAdmin CorePlugin 'QuickScript'
    being able to access the internal env.

--- CorePlugins/QuickScript.lua
+++ CorePlugins/QuickScript.lua
@@ -21,7 +21,53 @@ end
 
 command.Run = function(main,user,...)
 	local source = table.concat({...}," ")
-	local func,err = CoreAPI.LuaVM(source)
+	local env = {
+		--Instances
+		workspace = workspace;
+		game = game;
+		
+		--Classes
+		Instance = Instance;
+		Vector2 = Vector2;
+		Vector3 = Vector3;
+		Vector2int16 = Vector2int16;
+		Vector3int16 = Vector3int16;
+		BrickColor = BrickColor;
+		UDim = UDim;
+		UDim2 = UDim2;
+		math = math;
+		Enum = Enum;
+		string = string;
+		table = table;
+		
+		--Functions
+		pcall = pcall;
+		spawn = spawn;
+		xpcall = xpcall;
+		ypcall = ypcall;
+		getmetatable = getmetatable;
+		setmetatable = setmetatable;
+		type = type;
+		tick = tick;
+		print = print;
+		warn = warn;
+		error = error;
+		pairs = pairs;
+		
+		--Special-Case functions
+		setfenv = function(func,...)
+			if (type(func) == "number") then return { error = "Cannot use setfenv(number)"; } end
+			return setfenv(func,...)
+		end;
+		getfenv = function(func,...)
+			if (type(func) == "number") then return { error = "Cannot use getfenv(number)"; } end
+			return getfenv(func,...)
+		end;
+		
+		--SyncAPI
+		SyncAPI = SyncAPI;
+	}
+	local func,err = CoreAPI.LuaVM(source,env,true)
 	if (func) then
 		local fenv = getfenv(func)
 		fenv.script = nil
