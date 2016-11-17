ba7ab410ad5c918844200851665629e93d52bc8d
Skeleton <benjamindavidwright@googlemail.com>
2016-11-17 20:41:47 +0000

    Create Smoke.lua

---
+++ Custom Plugins/Smoke/Smoke.lua
@@ -0,0 +1,30 @@
+
+--[[
+									Verified SyncAdmin Plugin
+	======================================================================================
+	Authors 				Hannah Jane [DataSynchronized], Dominik [VolcanoINC]
+	--------------------------------------------------------------------------------------
+--]]
+
+local command = {}
+command.PermissionLevel = 1
+command.Shorthand = nil
+command.Params = {"PlayerList"}
+command.Usage = "smoke Player1,Player2,..."
+command.Description = [[Gives a player smoke]] 
+
+command.Init = function(main)
+end
+
+command.Run = function(main,user,users)
+	local list = {}
+	for _,player in pairs(users) do
+		if (player.Character) then
+			Instance.new("Smoke",player.Character:findFirstChild("HumanoidRootPart"))
+			table.insert(list,player.Name)
+		end
+	end
+	return true,"Gave the following people smoke: " .. table.concat(list,", "),list,user.Name .. " gave you smoke"
+end
+
+return command
