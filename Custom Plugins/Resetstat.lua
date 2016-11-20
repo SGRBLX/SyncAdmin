02d6d047db20682d0313203047cf19111b687bab
Zachary <Zlecktov@gmail.com>
2016-11-19 23:42:41 -0600

    Update Resetstat.lua
    Feeling kinda spunky and decided to go with it.  Instantly added support for the "all" stat command.   Heres 2 hoping they don't have a stat called all.

--- Custom Plugins/Resetstat.lua
+++ Custom Plugins/Resetstat.lua
@@ -3,11 +3,11 @@
 --      @Author: skeletonarcher12
 
 local command = {}
-command.PermissionLevel = 1 
-command.Shorthand = nil
-command.Params = {"PlayerList","String"} 
-command.Usage = "resetstats player leaderstat"
-command.Description = [[Resets the player's leaderstat. Only accepts IntValue and NumberValue]] 
+command.PermissionLevel = 1 -- Set the plugin permission level
+command.Shorthand = nil -- Set the plugin shorthand
+command.Params = {"PlayerList","String"} -- Set the plugin parameters
+command.Usage = "resetstat player leaderstat/all" -- Set the plugin usage
+command.Description = [[Resets the player's leaderstat(s). Only accepts IntValue, NumberValue, BoolValue, and StringValue]]  -- Set the plugin description
 
 command.Init = function(main)
 end
@@ -16,22 +16,36 @@ command.Run = function(main,user,users,stat)
 	local list = {}
 	for _,player in pairs(users) do
 		if player:FindFirstChild('leaderstats')~=nil then
-			if player.leaderstats:FindFirstChild(stat)~=nil then
-				local leadstat = player.leaderstats:FindFirstChild(stat)
-				
-				if leadstat:IsA("IntValue") then
-					leadstat.Value = 0
-				elseif leadstat:IsA("NumberValue") then
-					leadstat.Value = 0
-				elseif leadstat:IsA("StringValue") then
-					leadstat.Value = ""
-				elseif leadstat:IsA("BoolValue") then
-					leadstat.Value = false
+			if string.lower(stat)~='all' then
+				if player.leaderstats:FindFirstChild(stat)~=nil then
+					local leadstat = player.leaderstats:FindFirstChild(stat)
+					
+					if leadstat:IsA("IntValue") then
+						leadstat.Value = 0
+					elseif leadstat:IsA("NumberValue") then
+						leadstat.Value = 0
+					elseif leadstat:IsA("StringValue") then
+						leadstat.Value = ""
+					elseif leadstat:IsA("BoolValue") then
+						leadstat.Value = false
+					else
+						return false, "The type of leaderstat is invalid."
+					end
 				else
-					return false, "The type of leaderstat is invalid."
+					return false,"No leaderstat value matching name"
 				end
-			else
-				return false,"No leaderstat value matching name"
+			elseif string.lower(stat)=="all" then
+				for _,Stat in pairs(player.leaderstats:GetChildren()) do
+					if Stat:IsA("IntValue") then
+						Stat.Value = 0
+					elseif Stat:IsA("NumberValue") then
+						Stat.Value = 0
+					elseif Stat:IsA("StringValue") then
+						Stat.Value = ""
+					elseif Stat:IsA("BoolValue") then
+						Stat.Value = false
+					end
+				end
 			end
 		end
 		
